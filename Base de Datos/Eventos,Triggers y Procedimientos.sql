c216fd0879d1d6761f89466e475535882ab500e6
Moringos7 <moringos7@gmail.com>
2018-04-25 08:40:47 -0500

    Cambio triggers

--- Base de Datos/Eventos,Triggers y Procedimientos.sql
+++ Base de Datos/Eventos,Triggers y Procedimientos.sql
@@ -1,8 +1,25 @@
+--//---Por programar
+--
+--Verificacion Scouter diario
+CREATE EVENT VerificacionScouter ON SCHEDULE EVERY 1 DAY STARTS 
+'2018-04-25 07:45:00' ON COMPLETION  PRESERVE ENABLE DO 
+BEGIN
+	CALL ActualizacionScouter(); 
+END
+--Verificacion Comentario Anual
+--Verificacion Seccion por cumpleaños
 --Eventos--
 --Evento Actualización Contraseñas--
-CREATE EVENT `ActualizacionIntentosPassword` ON SCHEDULE EVERY 1 MONTH STARTS
-'2018-04-01 12:00:00.000000' ON COMPLETION NOT PRESERVE ENABLE DO Update password set Intentos = 3;
-
+DELIMITER //
+CREATE EVENT ActualizacionIntentosPassword ON SCHEDULE EVERY 1 MONTH STARTS 
+'2018-00-00 00:00:00' ON COMPLETION  PRESERVE ENABLE DO Update password set Intentos = 3;
+--Evento Actualización Inventario Posterior a un Servicio---
+DELIMITER //
+CREATE EVENT ReinicioInventario ON SCHEDULE EVERY 1 MONTH STARTS 
+'2018-00-00 10:00:00' ON COMPLETION PRESERVE ENABLE DO 
+BEGIN 
+CALL ResetInventario(); 
+END //
 
 --Triggers-- 
 --TriggerSeccionInsertar--
@@ -28,7 +45,7 @@ ELSE
 		SET new.Fkseccion = (SELECT idseccion from seccion where Nombre = 'dirigente'); 
 	END IF; 
 END IF;
-END
+END//
 DELIMITER ;
 
 --TriggerSeccionEditar--
@@ -54,12 +71,51 @@ ELSE
 		SET new.Fkseccion = (SELECT idseccion from seccion where Nombre = 'dirigente');
 	END IF;
 END IF; 
-END
+END//
 DELIMITER ;
 
 --Procedimientos--
----Actualizacion Comnetarios---
-CREATE PROCEDURE `ActualizacionComentarios`() NOT DETERMINISTIC CONTAINS SQL SQL SECURITY DEFINER 
+--Actualización Invetario--
+DELIMITER //
+CREATE PROCEDURE ResetInventario() NOT DETERMINISTIC CONTAINS SQL SQL SECURITY DEFINER 
+BEGIN
+DECLARE vnInventario INTEGER; 
+DECLARE vProducto VARCHAR(50);
+DECLARE vCantidad FLOAT;
+DECLARE vExistencia INTEGER;
+DECLARE vAM INTEGER; 
+DECLARE vAMD INTEGER;
+DECLARE i INTEGER;
+
+SET i = 1;
+SET vnInventario = (SELECT COUNT(*) FROM inventario where Extra = 0);
+SET vAM = (SELECT COUNT(*) FROM adultomayor);
+SET vAMD = (SELECT COUNT(*) FROM adultomayor where Diabetico = 1);
+
+while i <= vnInventario DO
+	SET vCantidad = (SELECT Cantidad from Inventario where IdInventario = i);
+	SET vProducto = (SELECT Producto FROM Inventario WHERE IdInventario = i);
+	SET vExistencia = (SELECT Existencia FROM Inventario WHERE IdInventario = i);
+
+	IF vCantidad > 1 THEN
+		SET vExistencia = (vExistencia - (vCantidad * vAM));
+	ELSEIF vProducto = 'Splenda' THEN
+		SET vExistencia = (vExistencia - vAMD);
+	ELSE
+		SET vExistencia = (vExistencia - vAM);
+	END IF;
+	IF vExistencia < 0 THEN
+		SET vExistencia = 0;
+	END IF;
+	UPDATE inventario SET Existencia = vExistencia WHERE IdInventario = i;
+	SET i = i+1;
+END while;
+
+END //
+--**********PENDIENTE REVISION**************--
+---Actualizacion Comentarios---
+DELIMITER //
+CREATE PROCEDURE ActualizacionComentarios() NOT DETERMINISTIC CONTAINS SQL SQL SECURITY DEFINER 
 BEGIN 
 DECLARE vIdMax Integer; 
 DECLARE vCont Integer DEFAULT 1;
@@ -76,7 +132,8 @@ WHILE vCont <= vIdMax DO
 END WHILE; 
 END
 ---Actualizacion Scouter----
-CREATE PROCEDURE `ActualizacionScouter`() NOT DETERMINISTIC CONTAINS SQL SQL SSECURITY DEFINER 
+DELIMITER //
+CREATE PROCEDURE ActualizacionScouter() NOT DETERMINISTIC CONTAINS SQL SQL SSECURITY DEFINER 
 BEGIN 
 DECLARE vIdMax Integer;
 DECLARE vCont Integer DEFAULT 1; 
@@ -91,4 +148,4 @@ WHILE vCont <= vIdMax DO
 	END IF;
 	SET vCont = vCont + 1;
 END WHILE; 
-END
+END//
