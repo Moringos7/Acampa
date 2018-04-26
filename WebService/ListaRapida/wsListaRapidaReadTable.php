d2a70a070094699cd333ae028f2e387f12ee81ab
Moringos7 <moringos7@gmail.com>
2018-04-26 13:51:32 -0500

    Implementacion  Lista Rapida

--- WebService/ListaRapida/wsListaRapidaReadTable.php
+++ WebService/ListaRapida/wsListaRapidaReadTable.php
@@ -1,31 +1,56 @@
 <?php 
 
+/*
+*------------*--------*--------*--------*-----------*------*
+|IdInventario|Producto|Cantidad|Faltante|Descripcion|Imagen|
+*/
 require("../wsBDcredencial.php");
 	$conexion = mysqli_connect($hostname,$username,$password,$database);
 	$queryI = "SELECT IdInventario,Producto,Cantidad,Existencia,Descripcion,Imagen FROM inventario where Extra = 0";
 	$queryAM = "SELECT count(*) FROM adultomayor";
-	$Inventario = array();
+	$queryAMD = "SELECT count(*) FROM adultomayor WHERE Diabetico = 1";
+	$json = array();
+	$Lista = array();
 
-	$resultadoI = mysqli_query($conexion,$queryI);
 	$resultadoam = mysqli_query($conexion,$queryAM);
-
+	$resultadoamd = mysqli_query($conexion,$queryAMD);
+	$resultadoI = mysqli_query($conexion,$queryI);
+	
 	while($registro = mysqli_fetch_array($resultadoam,MYSQLI_NUM)){
 		$nAdultoMayor = $registro[0];
 	}
-	
+	while($registro = mysqli_fetch_array($resultadoamd,MYSQLI_NUM)){
+		$nAdultoMayorDiabetico = $registro[0];
+	}
 	while($registro = mysqli_fetch_array($resultadoI,MYSQLI_ASSOC)){
-		$regis['Producto'] = utf8_encode($registro['Producto']);
-
+		$registro['Producto'] = utf8_encode($registro['Producto']);
 		$registro['Descripcion'] = utf8_encode($registro['Descripcion']);
 		$registro['Imagen'] = utf8_encode($registro['Imagen']);
 
-		$Inventario['Inventario'][] = $registro;
+		if($registro['Producto'] == "Splenda"){
+			$Faltante = $nAdultoMayorDiabetico - $registro['Existencia'];
+		}else if($registro['Cantidad'] == 2){
+			//echo "2";
+			$Faltante = ($nAdultoMayor * 2) - $registro['Existencia'];
+		}else {
+			//echo "1 o menos"
+			$Faltante = ($nAdultoMayor * 1) - $registro['Existencia'];
+		}
+		if($Faltante < 0){
+			$Faltante = 0;
+		}
+
+		$Lista['IdInventario'] = $registro['IdInventario'];
+		$Lista['Producto'] = $registro['Producto'];
+		$Lista['Cantidad'] = $registro['Cantidad'];
+		$Lista['Faltante'] = $Faltante;
+		$Lista['Descripcion'] = $registro['Descripcion'];
+		$Lista['Imagen'] = $registro['Imagen'];
+
+		$json['ListaRapida'][] = $Lista;
 	}
-	//var_dump($Inventario);
-	
-
 	mysqli_close($conexion);
-
+	echo json_encode($json);
 
 
 /*
