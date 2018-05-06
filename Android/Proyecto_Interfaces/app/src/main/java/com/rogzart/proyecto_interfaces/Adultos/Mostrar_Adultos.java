c387e93681b41a9df0478c627ab699819e2ae25f
DanCastell98 <fdanycast@gmail.com>
2018-05-06 15:48:49 -0500

    Canmbios a interfaz

---
+++ Android/Proyecto_Interfaces/app/src/main/java/com/rogzart/proyecto_interfaces/Adultos/Mostrar_Adultos.java
@@ -0,0 +1,31 @@
+package com.rogzart.proyecto_interfaces.Adultos;
+
+import android.os.Bundle;
+import android.support.design.widget.FloatingActionButton;
+import android.support.design.widget.Snackbar;
+import android.support.v7.app.AppCompatActivity;
+import android.support.v7.widget.Toolbar;
+import android.view.View;
+
+import com.rogzart.proyecto_interfaces.R;
+
+public class Mostrar_Adultos extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_mostrar__adultos);
+        Toolbar toolbar = (Toolbar) findViewById(R.id.toolbar);
+        setSupportActionBar(toolbar);
+
+        FloatingActionButton fab = (FloatingActionButton) findViewById(R.id.fab);
+        fab.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View view) {
+                Snackbar.make(view, "Replace with your own action", Snackbar.LENGTH_LONG)
+                        .setAction("Action", null).show();
+            }
+        });
+    }
+
+}
