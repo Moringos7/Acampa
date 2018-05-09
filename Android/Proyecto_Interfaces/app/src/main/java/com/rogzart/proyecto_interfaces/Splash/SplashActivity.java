25ca525b2855b41948535647255b4f57883f4b41
DanCastell98 <fdanycast@gmail.com>
2018-05-09 00:22:29 -0500

    Cambio estilos y animacion de splash

--- Android/Proyecto_Interfaces/app/src/main/java/com/rogzart/proyecto_interfaces/Splash/SplashActivity.java
+++ Android/Proyecto_Interfaces/app/src/main/java/com/rogzart/proyecto_interfaces/Splash/SplashActivity.java
@@ -6,27 +6,51 @@ import android.content.pm.ActivityInfo;
 import android.os.Bundle;
 import android.os.Handler;
 import android.view.WindowManager;
+import android.view.animation.Animation;
+import android.view.animation.AnimationUtils;
+import android.widget.ImageView;
+import android.widget.TextView;
 
 import com.rogzart.proyecto_interfaces.InterfacesLogin.Inicio;
 import com.rogzart.proyecto_interfaces.R;
 
-public class SplashActivity extends Activity{
-    private final int DURACION_SPLASH = 2000;
+import org.w3c.dom.Text;
+
+import static java.lang.Thread.sleep;
+
+public class SplashActivity extends Activity {
+    private TextView ivSplash;
+    private ImageView IS;
+
 
     @Override
-    public void onCreate (Bundle savedInstanceState){
+    public void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setRequestedOrientation(ActivityInfo.SCREEN_ORIENTATION_PORTRAIT);
         this.getWindow().setFlags(WindowManager.LayoutParams.FLAG_FULLSCREEN, WindowManager.LayoutParams.FLAG_FULLSCREEN);
 
         setContentView(R.layout.activity_splash);
 
-        new Handler().postDelayed(new Runnable() {
-            @Override
+        ivSplash = (TextView) findViewById(R.id.TS);
+        IS = (ImageView) findViewById(R.id.ivSplash);
+        Animation myanim = AnimationUtils.loadAnimation(this, R.anim.splash_transicion);
+        IS.startAnimation(myanim);
+        ivSplash.startAnimation(myanim);
+        final Intent intent = new Intent(this, Inicio.class);
+        Thread timer = new Thread() {
             public void run() {
-                Intent intent  = new Intent(SplashActivity.this, Inicio.class);
-                startActivity(intent);
-                finish();
-            };
-        },  DURACION_SPLASH); }
+                try {
+                    sleep(5000);
+                } catch (InterruptedException e) {
+                    e.printStackTrace();
+                } finally {
+                    startActivity(intent);
+                    finish();
+                }
+
+            }
+
+        };
+        timer.start();
+    }
 }
